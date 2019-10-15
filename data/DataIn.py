4ead8fcf6c6fa923c8d8f863e27bc7c806d5617d
MH Seo <14.73oo6o19@gmail.com>
2019-10-15 17:51:05 +0900

    Data_In

--- data/DataIn.py
+++ data/DataIn.py
@@ -1,12 +1,10 @@
 import urllib
 import xmltodict
 import json
-from pprint import pprint
 
 ServiceKey = "hZ5wN1oxmwnrr1aSfsKpi1XM8AhX3DGSYBkahybytwVOCZfDpDgfvrQmZkxHvEGfNd%2BgFEsDzQdl4BhjG%2BbU3Q%3D%3D"
-# url = 'http://apis.data.go.kr/1470000/HtfsInfoService/getHtfsItem'
-url = 'http://apis.data.go.kr/1470000/HtfsInfoService/getHtfsList
-
+url = 'http://apis.data.go.kr/1470000/HtfsInfoService/getHtfsItem' #상세정보조회
+# url = 'http://apis.data.go.kr/1470000/HtfsInfoService/getHtfsList' #목록조회
 queryParams = '?' + 'serviceKey=' + ServiceKey
 
 request = urllib.request.Request(url+queryParams)
@@ -15,9 +13,9 @@ rescode = response.getcode()
 if(rescode==200):
     response_body = response.read()
 #     print(response_body.decode('utf-8')) 
-    rD = xmltodict.parse(response_body)
-    rDJ = json.dumps(rD)
-    rDD = json.loads(rDJ)
-    print(rDD)
+    XML_dict = xmltodict.parse(response_body)
+    change_json = json.dumps(XML_dict)
+    json_dict = json.loads(change_json)
+    print(json_dict)
 else:
     print("Error Code:" + rescode)
