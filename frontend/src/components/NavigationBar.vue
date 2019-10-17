31139b03dc87b0bb750c65149ac1541c964dfd74
Ryuhojin <interarea@naver.com>
2019-10-17 17:35:37 +0900

    uiux : mainpage icon update, main logo update, detail ui fixed

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -2,22 +2,26 @@
   <div class="navigation_bar">
     <div class="navigation_menu" @click="goTo(0)">
       <span>검색</span>
+      <span class="line_span"></span>
     </div>
     <div class="navigation_menu" @click="goTo(1)">
       <span>랭킹</span>
+      <span class="line_span"></span>
     </div>
     <div class="navigation_menu" @click="goTo(2)">
       <span>이벤트</span>
+      <span class="line_span"></span>
     </div>
     <div class="navigation_menu" @click="goTo(3)">
       <span>어워드</span>
+      <span class="line_span"></span>
     </div>
   </div>
 </template>
 <script>
 import router from "../router";
 export default {
-  data: {},
+  
   methods: {
     goTo(n) {
       if (n == 0) {
@@ -33,20 +37,25 @@ export default {
 <style>
 .navigation_bar {
   width: 100%;
-  height: 6vh;
+  height: 6.5vh;
   display: flex;
   justify-content: space-around;
   align-items: center;
-  border-top: 1px solid red;
-  border-bottom: 1px solid red;
+  border-bottom: 1px solid lightgray;
 }
 
 .navigation_menu {
+  /* margin-top:5px;
+  margin-bottom: 5px; */
   width: 20%;
   height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   cursor: pointer;
+}
+.navigation_menu:hover{
+border-bottom: 3px solid #e85f63;
 }
 </style>
