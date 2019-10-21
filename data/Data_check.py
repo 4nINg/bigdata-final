c699678f4de05e62c764dbb49651a534e6a6c6bd
Hyeon Jin <choo0618@naver.com>
2019-10-21 09:08:34 +0900

    Data check

--- data/Data_check.py
+++ data/Data_check.py
@@ -8,12 +8,12 @@ for lst in rdr:
     if not lst[6] in Dic:
         Dic[lst[6]] = []
     Dic[lst[6]].append(lst[3])
-print(Dic['네이처바이트 종합비타민 위드 미네랄'])
-# print(Dic['비오틴'])
+print(Dic['헬스프랜드 프리미엄'])
 # print('총 종류: ',len(Dic))
 # cnt = 0
 # for key, value in Dic.items():
-#     if len(value)>1:
-#         cnt+=1
-#         print(key, value)
+#     print(key, ': ', value)
+    # if len(value)>1:
+    #     cnt+=1
+        # print(key, value)
 # print('회사 2개이상:' ,cnt)
