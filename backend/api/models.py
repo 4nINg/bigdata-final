0d83581faa58a185f7424b8fdba62327808ff2c0
HARRY LEE <sinyo0523@gmail.com>
2019-10-23 17:56:02 +0900

    feat: set the model for db

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.contrib.postgres.fields import JSONField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -27,3 +27,28 @@ def create_profile(**kwargs):
     )
 
     return profile
+
+class Ingredient(models.Model):
+    name = models.TextField()
+
+class Function(models.Model):
+    name = models.TextField()
+
+class Product(models.Model):
+    name = models.TextField() # 제품명
+    company_name = models.TextField() # 회사명
+    ingredient_list = JSONField() # 주성분 리스트
+    product_to_ingredient = models.ManyToManyField(Ingredient, related_name="ingredient_to_product") # 성분 M:N
+    product_to_function = models.ManyToManyField(Function, related_name="function_to_product") # 효능 M:N
+    heavy_metal = JSONField() # 중금속 리스트
+    intake_hint = models.TextField()  # 섭취힌트
+    intake_method = models.TextField() # 섭취방법
+    preservation = models.TextField() # 보존방법
+    distrbution = models.TextField() # 유통기한
+    image_url = models.TextField() # 이미지url
+    views = models.IntegerField(default=0) # 조회수
+
+
+# class Comment(models.Model):
+#     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
--- backend/api/views/auth_views.py
+++ backend/api/views/auth_views.py
@@ -13,7 +13,6 @@ from django.contrib.auth.models import User
 @api_view(['POST'])
 def signup(request):
     params = request.data.get('params', None)
-    print(params)
     email, password, nickname = params['email'], params['password'], params['nickname']
     age, gender = params['age'], params['gender']
 
@@ -37,7 +36,10 @@ def login(request):
             token = Token.objects.get(user=user)
         except Exception as e:
             token = Token.objects.create(user=user)
-        request.session[str(token)]= email
+        
+        token = str(token)
+        request.session[token] = email
+        
         profile = Profile.objects.get(user=user)
         login_info = {
             'username':email,
@@ -60,36 +62,37 @@ def login(request):
 # 로그인 세션 유지 / 유저 정보 저장
 @api_view(['POST'])
 def session(request):
-    params = request.data.get('params', None)
-    token = params['token']
-    email = request.session.get(token, None)
-    if email == None:
-        login_info = {
-            'username': None,
-            'token': None,
-            'is_authenticated': None,
-            'nickname': None,
-            }
-    else:
-        user = User.objects.get(username=email)
-        if user.is_authenticated and token == str(Token.objects.get(user=user)):
-            profile = Profile.objects.get(user=user)
-            login_info = {
-                'username':email,
-                'token':token,
-                'is_authenticated': True,
-                'nickname': profile.nickname,
-            }
-        else:
+    if request.method == 'POST':
+        token = request.data.get('token', None)
+        email = request.session.get(token, None)
+        print(email)
+        if email == None:
             login_info = {
                 'username': None,
                 'token': None,
                 'is_authenticated': None,
                 'nickname': None,
-            }
-    
-    serializer = ProfileSerializer(login_info)
-    return Response(data=serializer.data, status=status.HTTP_200_OK)
+                }
+        else:
+            user = User.objects.get(username=email)
+            if user.is_authenticated and token == str(Token.objects.get(user=user)):
+                profile = Profile.objects.get(user=user)
+                login_info = {
+                    'username':email,
+                    'token':token,
+                    'is_authenticated': True,
+                    'nickname': profile.nickname,
+                }
+            else:
+                login_info = {
+                    'username': None,
+                    'token': None,
+                    'is_authenticated': None,
+                    'nickname': None,
+                }
+        
+        serializer = ProfileSerializer(login_info)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 @api_view(['DELETE'])
 def logout(request):
